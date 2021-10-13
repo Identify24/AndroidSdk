aaa1761b61eaac0b0cabebff5bdf704a9777301c
Evren Ay <evrenay16@gmail.com>
2021-10-13 14:47:13 +0300

    - comment added

--- design/src/main/java/com/identify/design/signature/SignatureModuleFragment.kt
+++ design/src/main/java/com/identify/design/signature/SignatureModuleFragment.kt
@@ -16,9 +16,12 @@ class SignatureModuleFragment : BaseSignatureModuleFragment() {
 
     override fun getLayoutRes(): Int = R.layout.fragment_signature_module
 
+/*
+    Modül bitiminde araya girip ekran göstermek istenirse
+
     override fun signatureModuleFinished() {
         childFragmentManager.beginTransaction().replace(getFragmentContainer(),InformationDialogFragment.newInstance(identifyModuleTypes = IdentifyModuleTypes.DIJITAL_SIGNATURE,animResourceId = R.raw.signature,infoTitleText = getString(R.string.pls_sign),infoContentText = getString(R.string.pls_sign_desc)),InformationDialogFragment::class.java.toString()).commitAllowingStateLoss()
-    }
+    }*/
 
     companion object {
 
